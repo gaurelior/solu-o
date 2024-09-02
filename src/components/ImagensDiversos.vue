90cb0fecd15c7de260c66d7f14ee395c059d1449
gaurelior <132467593+gaurelior@users.noreply.github.com>
2024-09-02 11:47:07 -0300

    Add files via upload

---
+++ src/components/ImagensDiversos.vue
@@ -0,0 +1,31 @@
+<template>
+   <div class="containerImagem">
+    
+    <img v-lazy="imageSrc" 
+    alt="imagem 1" class="ImagemUnicaAltura400px">
+    </div>
+    <div class="divImagemMeiaTela">
+        <img src="@/assets/cafethe.jpg" alt="imagem 2">
+        <img src="@/assets/cafemaq.jpg" alt="imagem 3">  
+      </div>
+     
+      <picture>
+  <source srcset="image.webp" type="image/webp">
+  <img src="image1.jpg" alt="Descrição da imagem">
+</picture>
+
+    </template>
+    
+    <script> 
+     import {ref} from 'vue';
+const imageSrc = ref ("@/assets/cafegosto.jpg");
+    export default {
+        name: 'ImagensDiversos',
+        props: {
+            msg: String, imageSrc 
+        }
+    }
+    </script>
+    
+    <!--Add "scoped" atribute to limite css to this component only-->
+    <style scoped></style>
